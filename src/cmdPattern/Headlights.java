06120f56dd59fd101684f5531847494b0a78d218
sathvik <kotasathvikreddy@gmail.com>
2021-09-18 00:54:50 +0530

    headlight change

--- src/cmdPattern/Headlights.java
+++ src/cmdPattern/Headlights.java
@@ -2,10 +2,10 @@ package cmdPattern;
 
 public class Headlights {
 	  public  void switchOn() {
-	        System.out.println("Headlights are turned off");
+	        System.out.println("Headlights are turned on");
 	    }
 	 
 	    public void switchOff() {
-	        System.out.println("Headlights are turned on");
+	        System.out.println("Headlights are turned off");
 	    }
 }
